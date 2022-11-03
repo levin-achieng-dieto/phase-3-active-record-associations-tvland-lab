1dc5b008360626e4f01a99e96731ebd54608f3c3
levin-achieng-dieto <levin.dieto@student.moringaschool.com>
2022-11-03 15:20:30 +0300

    commit tree 102b09645da5f4d75235fead61509f897c0caecd

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,5 +1,9 @@
 class CreateActors < ActiveRecord::Migration[6.1]
   def change
     # Your code here
+    create_table :actors do |a|
+      a.string :first_name
+      a.string :last_name
+    end
   end
 end
--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,5 +1,10 @@
 class CreateCharacters < ActiveRecord::Migration[6.1]
   def change
     # Your code here
+    create_table :characters do |c|
+      c.string :name
+      c.integer :actor_id
+      c.integer :show_id
+    end
   end
 end
